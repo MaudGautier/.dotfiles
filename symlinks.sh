cc1a54a0fa84eed372c814691f7339c642c08a81
Maud Gautier <maud.j.gautier@gmail.com>
2020-03-29 20:46:39 +0200

    Link alias files

--- symlinks.sh
+++ symlinks.sh
@@ -13,6 +13,22 @@ ln -s ~/.dotfiles/zshenv ~/.zshenv
 ## Oh my zsh
 ln -s ~/.dotfiles/misc/maud.zsh-theme ~/.dotfiles/oh-my-zsh/custom/themes/maud.zsh-theme
 
+## Custom alias and function files for zsh
+ln -s ~/.dotfiles/custom/alias.zsh ~/.dotfiles/oh-my-zsh/custom/
+ln -s ~/.dotfiles/custom/functions.zsh ~/.dotfiles/oh-my-zsh/custom/
+
+## Cluster specific aliases
+## WARNING: Choose the right file
+configfiles=(
+	# ens-condor-config.zsh
+	# mac-to-pbil-config.zsh
+	# pbil-config.zsh
+	# pbil-pbs-config.zsh
+	)
+for config in ${configfiles[@]} ; do
+	ln -s ~/.dotfiles/custom/$config ~/.dotfiles/oh-my-zsh/custom/
+done
+
 ## Vim
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
