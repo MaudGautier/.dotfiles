c60e78aa35a5a7ac7a739b56f1daa498fdfb27ba
Maud Gautier <maud.j.gautier@gmail.com>
2020-04-07 22:48:53 +0200

    Automate homebrew installations

---
+++ brew.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# Installs dependencies needed using Homebrew.
+
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Please install Homebrew before running this script."
+  echo "  Homebrew can be installed following instructions from https://brew.sh/"
+  exit
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+FORMULAS=(
+R
+diff-pdf
+git
+java
+python3
+tree
+vim
+wget
+)
+
+CASKS=(
+1password
+Rstudio
+alfred
+brave-browser
+go2shell
+iterm2
+torbrowser
+vlc
+xquartz
+zotero
+)
+
+
+brew install ${FORMULAS[@]}
+
+brew cask install --appdir="/Applications" ${CASKS[@]}
+
+brew cleanup
+
+brew cask cleanup
+
+
