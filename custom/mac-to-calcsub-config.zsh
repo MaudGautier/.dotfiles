8e296df431c4bc6571d19f4c4574b2e00ab315f6
Maud Gautier <maud.j.gautier@gmail.com>
2020-04-20 11:18:20 +0200

    Add alias to calcsub connection

---
+++ custom/mac-to-calcsub-config.zsh
@@ -0,0 +1,6 @@
+# SSH to CALCSUB (Curie)
+alias csub='ssh mgautie3@calcsub.curie.fr'
+alias cs='csub'
+alias unm='umount /Users/maudgautier/OSXFUSE-calcsub/'
+alias mkmount='unm ; sshfs mgautie3@calcsub.curie.fr:/data/users/mgautie3 /Users/maudgautier/OSXFUSE-calcsub/'
+
