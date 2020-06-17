15320de1f6a2d767abed135002b055ab0041fab8
Maud Gautier <maud.j.gautier@gmail.com>
2020-06-17 13:51:25 +0200

    Add sshfs to scEwS remapping

--- custom/mac-to-calcsub-config.zsh
+++ custom/mac-to-calcsub-config.zsh
@@ -1,6 +1,6 @@
 # SSH to CALCSUB (Curie)
 alias csub='ssh mgautie3@calcsub.curie.fr'
 alias cs='csub'
-alias unm='umount /Users/maudgautier/OSXFUSE-calcsub/'
-alias mkmount='unm ; sshfs mgautie3@calcsub.curie.fr:/data/users/mgautie3 /Users/maudgautier/OSXFUSE-calcsub/'
+alias unm='umount /Users/maudgautier/OSXFUSE-calcsub/ ; umount /Users/maudgautier/OSXFUSE-calcsub-scEwS-remap-on-mouse-and-human/'
+alias mkmount='unm ; sshfs mgautie3@calcsub.curie.fr:/data/users/mgautie3 /Users/maudgautier/OSXFUSE-calcsub/ ; sshfs mgautie3@calcsub.curie.fr:/data/kdi_prod/.kdi/project_tmp_0/726/acl/26.01/ /Users/maudgautier/OSXFUSE-calcsub-scEwS-remap-on-mouse-and-human/'
 
