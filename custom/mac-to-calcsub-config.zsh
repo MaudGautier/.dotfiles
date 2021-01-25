03410c846ae9a190000fdf1fbd9bc9b9fb03738c
Maud Gautier <maud.j.gautier@gmail.com>
2021-01-25 16:10:32 +0100

    Mkmount to sotrel rather than to calcsub (now forbidden by admins)

--- custom/mac-to-calcsub-config.zsh
+++ custom/mac-to-calcsub-config.zsh
@@ -8,13 +8,14 @@ alias cs='csub'
 ### Create sshfs mountpoints
 mkmount () {
 	unm
-	sshfs -o reconnect mgautie3@calcsub.curie.fr:/data/users/mgautie3 /Users/maudgautier/OSXFUSE-calcsub/
-	sshfs -o reconnect mgautie3@calcsub.curie.fr:/data/kdi_prod/.kdi/project_tmp_0/726/acl/26.01/ /Users/maudgautier/OSXFUSE-calcsub-scEwS-remap-on-mouse-and-human/
-	sshfs -o reconnect mgautie3@calcsub.curie.fr:/data/kdi_prod/project_result/726/26.02/ /Users/maudgautier/OSXFUSE-calcsub-tumors-nadege/
-	sshfs -o reconnect mgautie3@calcsub.curie.fr:/data/kdi_prod/project_result/726/27.01/ /Users/maudgautier/OSXFUSE-calcsub-scallop-julien/
-	sshfs -o reconnect mgautie3@calcsub.curie.fr:/data/kdi_prod/project_result/726/27.02/ /Users/maudgautier/OSXFUSE-calcsub-arms-julien/
-	sshfs -o reconnect mgautie3@calcsub.curie.fr:/data/kdi_prod/project_result/726/27.03/ /Users/maudgautier/OSXFUSE-calcsub-dsrct-julien/
+	sshfs -o reconnect mgautie3@sotrel.curie.fr:/data/users/mgautie3 /Users/maudgautier/OSXFUSE-calcsub/
+	sshfs -o reconnect mgautie3@sotrel.curie.fr:/data/kdi_prod/.kdi/project_tmp_0/726/acl/26.01/ /Users/maudgautier/OSXFUSE-calcsub-scEwS-remap-on-mouse-and-human/
+	sshfs -o reconnect mgautie3@sotrel.curie.fr:/data/kdi_prod/project_result/726/26.02/ /Users/maudgautier/OSXFUSE-calcsub-tumors-nadege/
+	sshfs -o reconnect mgautie3@sotrel.curie.fr:/data/kdi_prod/project_result/726/27.01/ /Users/maudgautier/OSXFUSE-calcsub-scallop-julien/
+	sshfs -o reconnect mgautie3@sotrel.curie.fr:/data/kdi_prod/project_result/726/27.02/ /Users/maudgautier/OSXFUSE-calcsub-arms-julien/
+	sshfs -o reconnect mgautie3@sotrel.curie.fr:/data/kdi_prod/project_result/726/27.03/ /Users/maudgautier/OSXFUSE-calcsub-dsrct-julien/
 }
+# NB: mgautie3@sotrel.curie.fr is equivalent to : mgautie3@10.200.10.124
 
 ### Unmount sshfs connections
 unm () {
