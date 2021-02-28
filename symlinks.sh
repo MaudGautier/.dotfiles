ab2e3479b430400d653478682e38401b62c6a9ee
Maud Gautier <maudgautier@MacBook-Pro-de-Maud.local>
2021-02-28 22:12:23 +0100

    Rename oh-my-zsh to ohmyzsh

--- symlinks.sh
+++ symlinks.sh
@@ -11,11 +11,11 @@ ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/zshenv ~/.zshenv
 
 ## Oh my zsh
-ln -s ~/.dotfiles/misc/maud.zsh-theme ~/.dotfiles/oh-my-zsh/custom/themes/maud.zsh-theme
+ln -s ~/.dotfiles/misc/maud.zsh-theme ~/.dotfiles/ohmyzsh/custom/themes/maud.zsh-theme
 
 ## Custom alias and function files for zsh
-ln -s ~/.dotfiles/custom/alias.zsh ~/.dotfiles/oh-my-zsh/custom/
-ln -s ~/.dotfiles/custom/functions.zsh ~/.dotfiles/oh-my-zsh/custom/
+ln -s ~/.dotfiles/custom/alias.zsh ~/.dotfiles/ohmyzsh/custom/
+ln -s ~/.dotfiles/custom/functions.zsh ~/.dotfiles/ohmyzsh/custom/
 
 ## Cluster specific aliases
 ## WARNING: Choose the right file
@@ -26,7 +26,7 @@ configfiles=(
 	# pbil-pbs-config.zsh
 	)
 for config in ${configfiles[@]} ; do
-	ln -s ~/.dotfiles/custom/$config ~/.dotfiles/oh-my-zsh/custom/
+	ln -s ~/.dotfiles/custom/$config ~/.dotfiles/ohmyzsh/custom/
 done
 
 ## Vim
